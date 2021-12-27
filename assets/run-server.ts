ed31df1c5ca8dde4c7a48ab605cde3e49fca7b39
seveibar <seveibar@gmail.com>
2021-12-26 22:54:07 -0800

    start run server

--- assets/run-server.ts
+++ assets/run-server.ts
@@ -1,6 +1,20 @@
 import routes from "./routes"
 import micro from "micro"
+import getPort from "get-port"
 
-export const runServer = async ({}) => {}
+export const runServer = async ({
+  port,
+  staticDir = "./.next",
+  middlewares = [],
+}) => {
+  const server = micro((req, res) => {
+    res.end("hello world")
+  })
+  server.listen({ port })
+}
+
+if (!module.parent) {
+  runServer({ port: 3000 })
+}
 
 export default runServer
