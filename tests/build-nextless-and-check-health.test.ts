b9a6d92257bdef697f917ab61aa60ec360db6a7d
Itelo Filho <itelofilho@gmail.com>
2023-07-28 11:17:00 -0300

    feat: generate files for tests using --nextless and --skip-build together

---
+++ tests/build-nextless-and-check-health.test.ts
@@ -0,0 +1,16 @@
+import test from "ava"
+import getPort from "get-port"
+import axios from "axios"
+import { getSampleProject } from "./fixtures/get-sample-project-without-next"
+
+test("build and check health", async (t) => {
+  const {getNsmIndex} = await getSampleProject()
+  const port = await getPort()
+  const server = await getNsmIndex({ port })
+
+  const { data: res } = await axios.get(`http://localhost:${port}/api/health`)
+
+  t.truthy(res.includes("healthy!"))
+
+  server.close()
+})
